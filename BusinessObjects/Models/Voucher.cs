b6df874833b507ceb4a7d27a51a144bb3a1f79e3
Pham Manh Hung <hung080104@gmail.com>
2025-07-21 12:26:16 +0700

    Add Table VoucherCustomer & Update Voucher Quantity

--- BusinessObjects/Models/Voucher.cs
+++ BusinessObjects/Models/Voucher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BusinessObjects.Models;
 
@@ -10,14 +11,14 @@ public partial class Voucher
     public string? Code { get; set; }
 
     public int Vpercent { get; set; }
-
-    public int Quantity { get; set; }
+    [Range(0, int.MaxValue)]
+    public int? Quantity { get; set; }
 
     public DateTime? StartDate { get; set; }
 
     public DateTime? EndDate { get; set; }
 
-    public int? UserId { get; set; }
-
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<VoucherCustomer>? VoucherCustomers { get; set; } = null;
 }
